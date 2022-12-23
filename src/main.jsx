40186ee803361f1af4878d87de1bf7ef4b9bf83f
marti <martinreyesrf@gmail.com>
2022-12-22 21:23:04 -0600

    primer commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Portafolio } from './Portafolio'
+import './styler.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Portafolio/>
+  </React.StrictMode>,
+)
---
+++ src/views/LadingView.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './LadingfViewStyler.css'
+export const LadingView = () => {
+  return (
+    <section className='lading__main'>
+
+    </section>
+  )
+}
